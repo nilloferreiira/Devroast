ccf28c6c08f43713da852ba97ebaf7f42aa1a53b
Danillo Ferreira <nilloferreiira@gmail.com>
2026-03-14 13:23:22 -0300

    feat: biome setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "",
-	description: "",
+  title: "DevRoast",
+  description: "DevRoast application built with Next.js.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-	return (
-		<>
-			<h1>DevRoast</h1>
-		</>
-	)
+  return (
+    <>
+      <h1>DevRoast</h1>
+    </>
+  );
 }
